ee6ad0fa84ca415a07d60e382930306609db0aca
Alexandre Oliveira <ao.sanches.ao@gmail.com>
2023-08-10 18:49:20 -0300

    Criação do sistema de entrada de dados de pedidos

---
+++ Order_Summary/Entities/Client.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order_Summary.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDay { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(string name, string email, DateTime birthDay)
+        {
+            Name = name;
+            Email = email;
+            BirthDay = birthDay;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", ("
+                + BirthDay.ToString("dd/MM/yyyy")
+                + ") - "
+                + Email;
+        }
+    }
+}
